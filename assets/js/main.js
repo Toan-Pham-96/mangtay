82a07a1501f5447ae0cebbb670cf920f156a4d78
toan <phamsongtoan15091996@gmail.com>
2022-02-10 14:42:27 +0700

    fix-loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
         
 $(document).ready(function() {
 
+  //LOADING
+ 
+  if($(window).width() > 767){
+    setTimeout(function(){
+      $('body').addClass('loaded');
+    }, 1200);
+  } else{
+    setTimeout(function(){
+      $('body').addClass('loaded');
+    }, 0);
+
+  }
+
 
   //MENU
   $('.toggle-menu').click(function () {
@@ -19,11 +32,10 @@ $(document).ready(function() {
   });
 
   //TOGGLE
-
-$('#form-modal').submit( function(e) {
-  e.preventDefault()
-  }
-)
+  $('#form-modal').submit( function(e) {
+    e.preventDefault()
+    }
+  )
 
 
 $(".nav-menu ul li").click(function(e){
@@ -47,8 +59,6 @@ $(".nav-menu ul li").click(function(e){
 
   }
 
-
-
 })
 
   $(".nav-menu ul li").hover(function(){
@@ -68,9 +78,6 @@ $(".nav-menu ul li").click(function(e){
   });
 
 
-
-
-
     $(window).scroll(function(e) {
       const scrollTop = $(this).scrollTop();
       if (scrollTop > 30) {
@@ -80,32 +87,6 @@ $(".nav-menu ul li").click(function(e){
       }
   });
 
-  
-    // //SLIDE BANNER
-    // $('.banner-img__slide').slick({
-    //     autoplay: true,
-    //     autoplaySpeed: 3500,
-    //     slidesToScroll: 1,
-    //     slidesToShow: 1,
-    //     lazyLoad: 'ondemand',
-    //     nextArrow: false,
-    //     prevArrow: false,
-    //     speed: 1500,
-    //     asNavFor: '.banner-content__slide'
-
-    // });
-    //   //SLIDE BANNER
-    //   $('.banner-content__slide').slick({
-    //     autoplay: true,
-    //     autoplaySpeed: 3500,
-    //     speed: 1500,
-    //     slidesToScroll: 1,
-    //     slidesToShow: 1,
-    //     lazyLoad: 'ondemand',
-    //     nextArrow: false,
-    //     prevArrow: false,
-    //     asNavFor: '.banner-img__slide',
-    // });
     
 //SLIDE PARTNER
 $('.partner-slide-1').slick({
@@ -200,50 +181,6 @@ $('.cover-item').slick({
 
 });
 
-
-// // SPITLING
-//     // Cắt chữ bằng Splitting.js, mỗi chữ cái sẽ nằm trong 
-// // thẻ span với class="char"
-// new Splitting({ target: ".chozui"});
-
-// // Tạo hiệu ứng chữ chuyển động bằng anime.js 
-// anime({
-// 	targets: ".chozui .tdt",
-// 	duration: 750,
-// 	translateY: ["1.1em", 0],
-// 	translateX: ["0.5em", 0],
-// 	rotateZ: [180, 0],
-// 	opacity: [0, 1],
-// 	easing: "easeOutExpo",
-// 	delay: (el, i) => 50 * i,
-// 	loop: true
-// })
-
-
-
-// //ANIME.JS
-// // Wrap every letter in a span
-// var textWrapper = document.querySelector('.ml2');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.ml2 .letter',
-//     scale: [4,1],
-//     opacity: [0,1],
-//     translateZ: 0,
-//     easing: "easeOutExpo",
-//     duration: 950,
-//     delay: (el, i) => 70*i
-//   }).add({
-//     targets: '.ml2',
-//     opacity: 0,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-//   });
-
-
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -275,13 +212,3 @@ for (i = 0; i < acc.length; i++) {
         $(".heading-main").find(".nav-menu").removeClass("open");
       }
     });
-
-
-      // SPITLING
-
-  console.clear();
-  var s = Splitting();
-
-
-
-
